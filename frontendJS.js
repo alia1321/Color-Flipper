28026aa559b6ab54ce81398bec59eab6439d8959
alia1321 <aliaprofessionalmail@gmail.com>
2023-08-15 11:14:33 -0400

    final draft. Far from perfect but for this exercise it works well enough

--- frontendJS.js
+++ frontendJS.js
@@ -1,5 +1,6 @@
 const mainButton= document.getElementsByClassName("customize-btn")[0];
 const backgroundColor = document.getElementById("body");
+const mainHeading = document.getElementById("main-heading");
 
 
 
@@ -8,6 +9,7 @@ mainButton.addEventListener("click", async()=>{
         const response = await fetch("http://localhost:4000/");
         const randomHex = await response.text();
         backgroundColor.style.backgroundColor = randomHex;
+        mainHeading.textContent = "Background Color: " + randomHex;
 
     } 
     
--- frontendJS2.js
+++ frontendJS2.js
@@ -1,10 +1,11 @@
 const colors = ["silver","gray","white","maroon","red","purple","fuchsia","green","lime","olive","yellow", "navy","blue","teal", "aqua"];
 const mainButton= document.getElementsByClassName("customize-btn")[0];
 const backgroundColor = document.getElementById("body");
-
+const mainHeading = document.getElementById("main-heading");
 
 mainButton.addEventListener("click", () => {
     const currentColorIndex = Math.floor(Math.random() * colors.length);
     const currentColor = colors[currentColorIndex];
     backgroundColor.style.backgroundColor = currentColor;
+    mainHeading.textContent = "Background Color: " + currentColor
 });
